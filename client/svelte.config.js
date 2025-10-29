5e7fdf2150b06b9b96c89199e31bb229252ad808
Cattn <Logan@cattn.dev>
2025-10-28 23:45:15 -0400

    feat: use adapter-extension
    Use my custom adapter-extension package instead of a clunky postbuild script.

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@cattn/adapter-extension';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
