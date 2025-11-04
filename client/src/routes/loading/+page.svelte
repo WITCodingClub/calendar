a6b8c1fb8e1b33df14b7e2efa618a849f146ee02
Daniel Miretsky <miretskyd@wit.edu>
2025-11-04 18:52:20 -0500

    improve error messages

--- client/src/routes/loading/+page.svelte
+++ client/src/routes/loading/+page.svelte
@@ -80,13 +80,13 @@
 
             signIn();
         } catch (err) {
-            error = 'Sign in failed!';
+            error = 'Failed to fetch data! Make';
         }
     }
 
     async function signIn() {
         try {
-            const response = await fetch('https://heron-selected-literally.ngrok-free.app/api/user/onboard', {
+            const response = await fetch('https://heron-selected-lfgeetrewtegfgdgfdfgdfiterally.ngrok-free.app/api/user/onboard', {
                 method: 'POST',
                 body: JSON.stringify({email: schoolEmail}),
                 headers: {
@@ -110,7 +110,7 @@
             await new Promise(resolve => setTimeout(resolve, 3000));
             goto('/onboard');
         } catch (err) {
-            error = 'Server is (probably) down! Please email mayonej@wit.edu!';
+            error = 'Server is (probably) down! Please email mayonej@wit.edu! Also, make';
         }
     }
 </script>
@@ -118,8 +118,8 @@
 <div class="flex flex-col items-center justify-center min-h-screen w-full px-4">
     <div class=" rounded-lg shadow-md p-8 flex flex-col items-center peak bg-surface-container-high">
         {#if error}
-            <!-- <h1 class="text-3xl font-extrabold text-center text-error mb-4">Failed to sign in!</h1> -->
-            <p class="text-center text-error-container mb-4 break">{error} Make sure you're logged in to <a class="text-primary underline" href="https://selfservice.wit.edu/StudentRegistrationSsb/ssb/registrationHistory/registrationHistory" target="_blank">WIT Self Service</a>.</p>
+            <h1 class="text-3xl font-extrabold text-center text-error mb-4">Failed to sign in!</h1>
+            <p class="text-center text-error-container mb-4 break">{error} sure you're logged in to <a class="text-primary underline" href="https://selfservice.wit.edu/StudentRegistrationSsb/ssb/registrationHistory/registrationHistory" target="_blank">WIT Self Service</a>.</p>
             <Button variant="outlined" onclick={fetchSchoolEmail}>Try Again</Button>
         {:else}
             <h1 class="text-3xl font-extrabold text-center text-primary mb-6">Signing in!</h1>
