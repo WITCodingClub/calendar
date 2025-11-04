d05fabc66b7c9892815762de94d2b2d4f796d223
Jasper Mayone <mayonej@wit.edu>
2025-11-03 22:34:09 -0500

    Create types

---
+++ client/src/lib/types/Professor.ts
@@ -0,0 +1,6 @@
+export interface Professor {
+    first_name: string;
+    last_name: string;
+    email: string;
+    rmp_id?: string;
+}
---
+++ client/src/lib/types/ResponseData.ts
@@ -0,0 +1,7 @@
+import { type Course } from './Course';
+
+// todo: use this!
+export interface ResponseData {
+    ics_url: string;
+    classes: Course[];
+}
