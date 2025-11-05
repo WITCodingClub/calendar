948225d900043e17f064e1d463f6021144456d74
Daniel Miretsky <miretskyd@wit.edu>
2025-11-04 19:00:25 -0500

    Better error message when not signed in (#11)
    * Better error message when not signed in
    
    * improve error messages
    
    * revert temp server link change

--- client/src/routes/loading/+page.svelte
+++ client/src/routes/loading/+page.svelte
@@ -80,7 +80,7 @@
 
             signIn();
         } catch (err) {
-            error = 'Sign in failed!';
+            error = 'Failed to fetch data! Make';
         }
     }
 
@@ -110,7 +110,7 @@
             await new Promise(resolve => setTimeout(resolve, 1500));
             goto('/onboard');
         } catch (err) {
-            error = 'Server is (probably) down! Please email mayonej@wit.edu!';
+            error = 'Server is (probably) down! Please email mayonej@wit.edu! Also, make';
         }
     }
 </script>
@@ -119,7 +119,7 @@
     <div class=" rounded-lg shadow-md p-8 flex flex-col items-center peak bg-surface-container-high">
         {#if error}
             <h1 class="text-3xl font-extrabold text-center text-error mb-4">Failed to sign in!</h1>
-            <p class="text-center text-error-container mb-4 break">{error} Also, make sure you're logged in to <a class="text-primary underline" href="https://selfservice.wit.edu/StudentRegistrationSsb/ssb/registrationHistory/registrationHistory" target="_blank">WIT Self Service</a>.</p>
+            <p class="text-center text-error-container mb-4 break">{error} sure you're logged in to <a class="text-primary underline" href="https://selfservice.wit.edu/StudentRegistrationSsb/ssb/registrationHistory/registrationHistory" target="_blank">WIT Self Service</a>.</p>
             <Button variant="outlined" onclick={fetchSchoolEmail}>Try Again</Button>
         {:else}
             <h1 class="text-3xl font-extrabold text-center text-primary mb-6">Signing in!</h1>
