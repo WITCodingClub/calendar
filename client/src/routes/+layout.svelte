61634a118d6b806a8461efbc7a50933e08dcf070
Cattn <Logan@cattn.dev>
2025-11-08 23:29:48 -0500

    fix: flashbang

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -4,7 +4,10 @@
 
 	let { children } = $props();
 	import { NewSnackbar } from 'm3-svelte';
+	import { HDRRouter } from '@cattn/hdr';
+
 </script>
+<HDRRouter />
 
 <svelte:head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
