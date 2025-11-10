b95b428eb90326d0acfccbbd91595c552d598c86
Cattn <Logan@cattn.dev>
2025-11-09 19:15:14 -0500

    settings toggle
    A small button in settings purely to make debugging gcal easier

--- client/src/lib/components/Settings.svelte
+++ client/src/lib/components/Settings.svelte
@@ -4,15 +4,21 @@
     import { API } from "$lib/api";
     import { onMount } from "svelte";
 	import { goto } from "$app/navigation";
-import { userSettings as storedUserSettings } from "$lib/store";
-import { processedData as storedProcessedData } from "$lib/store";
+    import { userSettings as storedUserSettings } from "$lib/store";
+    import { processedData as storedProcessedData } from "$lib/store";
+    import { snackbar } from "m3-svelte";
 
     let userSettings = $state<UserSettings | undefined>(undefined);
     let email = $state<string | undefined>(undefined);
+    let emailToSignInWith: string | null = $state(null);
+    let emailToSubmit = $state('');
+    let jwt_token: string | undefined = $state(undefined);
 
     onMount(async () => {
+        jwt_token = await API.getJwtToken();
         userSettings = await API.userSettings();
 		storedUserSettings.set(userSettings);
+        tryForEmail();
         email = await API.getUserEmail().then(data => data.email);
     });
 
@@ -53,6 +59,50 @@ import { processedData as storedProcessedData } from "$lib/store";
         storedUserSettings.set(undefined);
         storedProcessedData.set([]);
         await goto('/');
+    }
+
+    async function tryForEmail() {
+        const info = await chrome.identity.getProfileUserInfo();
+        if (info && info.email) {
+            emailToSignInWith = info.email;
+        }
+    }
+
+    async function submitEmail() {
+        const emailToUse = emailToSignInWith || emailToSubmit; 
+        
+        const response = await fetch(`${API.baseUrl}/user/gcal`, {
+            method: 'POST',
+            body: JSON.stringify({email: emailToUse}),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt_token}`
+            }
+        });
+        const data = await response.json();
+        if (response.ok && data.oauth_url) {
+            const screenWidth = window.screen.availWidth;
+            const screenHeight = window.screen.availHeight;
+
+            await chrome.windows.create({
+                url: data.oauth_url,
+                width: 650,
+                height: 800,
+                left: Math.floor((screenWidth - 650) / 2),
+                top: Math.floor((screenHeight - 800) / 2),
+                type: 'popup'
+            });
+        } else if (response.ok && !data.oauth_url) {
+            await chrome.storage.local.set({
+                    oauth_status: 'success',
+            });
+            await chrome.storage.local.set({
+                oauth_email: emailToSignInWith || emailToSubmit,
+            });
+            goto('/calendar');
+        } else {
+            snackbar('Failed to submit email: ' + data.error, undefined, true);
+        }
     }
 </script>
 
@@ -116,6 +166,11 @@ import { processedData as storedProcessedData } from "$lib/store";
         </div>
     </div>
     <div class="flex flex-col gap-2 items-center justify-center mt-6 w-full">
+        <div class="flex justify-center mt-3 peak flex-col gap-2">
+            {#if emailToSignInWith}
+                <Button variant="filled" square onclick={submitEmail}>Continue with {emailToSignInWith}</Button>
+            {/if}
+        </div>
         <Button variant="filled" onclick={clearLocalStorage}>Clear Local Data</Button>
         <p class="text-sm text-error text-center max-w-md">
             <span class="font-semibold">Warning:</span>
