5dc655fbe974c953022ecf9ee8a1a97c0086ba44
Daniel Miretsky <miretskyd@wit.edu>
2026-01-04 22:36:14 -0500

    fix refresh courses button

--- client/src/routes/calendar/+page.svelte
+++ client/src/routes/calendar/+page.svelte
@@ -914,7 +914,7 @@
                 {#if processedData}
                     <Button variant="tonal" onclick={() => refreshSchedule(selected)} disabled={refreshing || loading}>
                         {#if refreshing}
-                            <LoadingIndicator size={20} />
+                            {refreshing ? 'Refreshing...' : 'Refresh'}
                         {:else}
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
