74f94b2f362f92bc19689039b280f370e32cc272
Logan <Logan@cattn.dev>
2026-01-06 13:21:56 -0500

    fix

--- client/src/routes/calendar/+page.svelte
+++ client/src/routes/calendar/+page.svelte
@@ -854,7 +854,7 @@
         <div class="w-full flex flex-col items-center gap-6 p-6 bg-surface-container-lowest rounded-md shadow-md border border-outline-variant max-w-lg mx-auto">
             <div class="flex flex-col gap-1 items-center">
                 <div class="flex items-center w-full justify-center relative">
-                    <div class="absolute left-0">
+                    <div class="absolute left-0 unpeak">
                         <Button variant="tonal" square onclick={async () => { await goto('/'); }} >
                             <span class="flex flex-row gap-2 items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9.184 4.457c.3.286.311.76.026 1.06L3.75 11.25H22a.75.75 0 0 1 0 1.5H3.75l5.46 5.733a.75.75 0 1 1-1.086 1.034l-6.667-7a.75.75 0 0 1 0-1.034l6.667-7a.75.75 0 0 1 1.06-.026"/></svg>
@@ -1153,5 +1153,9 @@
 
     :global(.peak button) {
         height: 2.5rem !important;
+    }
+
+    :global(.unpeak button) {
+        transform: scale(0.79);
     }
 </style>
